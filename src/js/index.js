eb8302e286c3824fe9c1cf27fba003cfa98c5834
Simon <simondereje1@gmail.com>
2023-12-22 11:16:36 +0300

    Added Tailwind CSS

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the mobile menu visibility
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
